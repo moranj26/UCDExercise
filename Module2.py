aac306cb95877dccffc87d86bd17ad4c224b3df6
Jason <moranj26@hotmail.com>
2021-03-13 18:01:43 +0000

    Module 1 - Chapter 2

--- Module2.py
+++ Module2.py
@@ -1,1 +1,7 @@
-Test2
+import requests
+
+request=requests.get('http://api.open-notify.org/')
+
+print(requests.status_code)
+
+print(request.text)
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-# This is a sample Python script.
+import requests
+
+request=requests.get('http://api.open-notify.org/')
+
+print(request.status_code)
+
+print(request.text)
+
+data=requests.get("http://api.open-notify.org/iss-now.json")
+
+print(data.text)
+
+print(data.status_code)
 
 
 
