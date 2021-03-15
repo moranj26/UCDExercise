ee0e052743904f31dec1ef4c307074e96c7bec47
Jason <moranj26@hotmail.com>
2021-03-15 22:42:19 +0000

    Module 3 - Upload 1

---
+++ Module3-Chapter1.py
@@ -0,0 +1,81 @@
+# Print the head of the homelessness data
+print(homelessness.head())
+
+# Print information about homelessness
+print(homelessness.info())
+
+# Print the shape of homelessness
+print(homelessness.shape)
+
+# Print a description of homelessness
+print(homelessness.describe())
+
+# Import pandas using the alias pd
+import pandas as pd
+
+# Print the values of homelessness
+print(homelessness.values)
+
+# Print the column index of homelessness
+print(homelessness.columns)
+
+# Print the row index of homelessness
+print(homelessness.index)
+
+# Sort homelessness by region, then descending family members
+homelessness_reg_fam = homelessness.sort_values(["region", "family_members"], ascending=[True, False])
+
+# Print the top few rows
+print(homelessness_reg_fam.head())
+
+# Select only the individuals and state columns, in that order
+ind_state = homelessness[["individuals", "state"]]
+
+# Print the head of the result
+print(ind_state.head())
+
+# Subset for rows in South Atlantic or Mid-Atlantic regions
+south_mid_atlantic = homelessnes([homelessness.isin["South Atlantic"] | ["Mid-Atlantic"]])
+
+# See the result
+print(south_mid_atlantic)
+
+# Subset for rows in South Atlantic or Mid-Atlantic regions
+south_mid_atlantic = homelessness[(homelessness["region"] == "South Atlantic") |(homelessness["region"] == "Mid-Atlantic")]
+
+# See the result
+print(south_mid_atlantic)
+
+# The Mojave Desert states
+canu = ["California", "Arizona", "Nevada", "Utah"]
+
+# Filter for rows in the Mojave Desert states
+mojave_homelessness = homelessness[homelessness["state"].isin(canu)]
+
+# See the result
+print(mojave_homelessness)
+
+# Add total col as sum of individuals and family_members
+homelessness ["total"] = homelessness["individuals"] + homelessness["family_members"]
+
+# Add p_individuals col as proportion of individuals
+homelessness ["p_individuals"] = homelessness["individuals"] / homelessness["total"]
+
+# See the result
+print(homelessness)
+
+# Create indiv_per_10k col as homeless individuals per 10k state pop
+homelessness["indiv_per_10k"] = 10000 * homelessness["individuals"] / homelessness["state_pop"]
+
+# Subset rows for indiv_per_10k greater than 20
+high_homelessness = homelessness[homelessness["indiv_per_10k"] > 20]
+
+# Sort high_homelessness by descending indiv_per_10k
+high_homelessness_srt = high_homelessness.sort_values("indiv_per_10k", ascending=False)
+
+# From high_homelessness_srt, select the state and indiv_per_10k cols
+result = high_homelessness_srt[["state", "indiv_per_10k"]]
+
+# See the result
+print(result)
+
